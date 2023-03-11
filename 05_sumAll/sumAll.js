d29e19b5e25774fc516195e6c915824a50f5c5b1
michael-hoang <mike081522@gmail.com>
2023-03-11 13:18:24 -0800

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (typeof num1 !== "number" || typeof num2 !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (num1 > num2) {
+        let container = num1;
+        [num1, num2] = [num2, container];
+    }
+    while (num1 <= num2) {
+        sum += num1;
+        num1++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
