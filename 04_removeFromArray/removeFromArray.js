91e04f15678650e3765aad7dad8fc5d392b2f07c
michael-hoang <mike081522@gmail.com>
2023-03-11 12:55:20 -0800

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...elements) {
+    if (!Array.isArray(array)) {
+        return "ERROR";
+    }
 
+    for (let element of elements) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === element) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
