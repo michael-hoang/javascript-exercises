7ffb124a69f6ad530f13fd6696e04eb26f1c8352
michael-hoang <mike081522@gmail.com>
2023-03-11 12:03:23 -0800

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (word) {
+    let reversed_word = "";
+    for (i = word.length - 1; i >= 0; i--) {
+        reversed_word += word[i];
+    }
+    return reversed_word;
 };
 
 // Do not edit below this line
