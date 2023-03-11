382a7db28b79faea285bb4b1fc65507c72f1db60
michael-hoang <mike081522@gmail.com>
2023-03-11 11:52:29 -0800

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, number) {
+    if (number < 0) {
+        return "ERROR";
+    } else {
+        string = "";
+        for (let i = 0; i < number; i++) {
+            string += word;
+        }
+        return string;
+    }
 };
 
 // Do not edit below this line
