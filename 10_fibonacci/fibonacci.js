453108fc9045e5d305c6ed11e77848e36ee2ef0e
michael-hoang <mike081522@gmail.com>
2023-04-09 18:55:55 -0700

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
 
+    let fib = [];
+    for (let i = 0; i < number; i++) {
+        if (i < 2) {
+            fib[i] = 1;
+        } else {
+            fib[i] = fib[i-2] + fib[i-1];
+        }
+    }
+    return fib[number - 1];
 };
 
 // Do not edit below this line
