9fee428baf8d006ca634d629aaa03770e850eb43
michael-hoang <mike081522@gmail.com>
2023-04-09 18:26:40 -0700

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = '';
+    for (char of string) {
+        const ascii = char.charCodeAt(0) ;
+        if (ascii > 64 && ascii < 91 || ascii > 96 && ascii < 123) {
+            newString +=char;
+        }
+    }
+    let reverseString = newString.split('').reverse().join('');
+    if (newString.toLowerCase() === reverseString.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
