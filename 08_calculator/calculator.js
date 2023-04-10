a8f115003c9de811eabd2ee795092d472eca0787
michael-hoang <mike081522@gmail.com>
2023-04-09 17:37:56 -0700

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  for (let number of numbers) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  for (let number of numbers) {
+    product *= number;
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a > 0) {
+    let b = factorial(a - 1);
+    return a * b;
+  }
+  return 1;
 };
 
 // Do not edit below this line
