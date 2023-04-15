fe554edeb0ea100e46cb06708861b8eaa24a21bc
michael-hoang <mike081522@gmail.com>
2023-04-15 10:30:39 -0700

    Update getTheTitles to use an array built-in method 'map'

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,14 @@
+// const getTheTitles = function(books_arr) {
+//     let books = [];
+//     for (let book of books_arr) {
+//         books.push(book['title'])
+//     }
+//     return books;
+// };
+
 const getTheTitles = function(books_arr) {
-    let books = [];
-    for (let book of books_arr) {
-        books.push(book['title'])
-    }
-    return books;
-};
+     return books_arr.map((book) => book.title);
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
