4ce237a97702e43623d284a85959228512b89bfb
michael-hoang <mike081522@gmail.com>
2023-04-15 09:49:30 -0700

    Update fibonnaci algorithm

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,31 @@
-const fibonacci = function(number) {
-    if (number < 0) {
-        return "OOPS";
-    }
+// const fibonacci = function(number) {
+//     if (number < 0) {
+//         return "OOPS";
+//     }
 
-    let fib = [];
-    for (let i = 0; i < number; i++) {
-        if (i < 2) {
-            fib[i] = 1;
-        } else {
-            fib[i] = fib[i-2] + fib[i-1];
-        }
+//     let fib = [];
+//     for (let i = 0; i < number; i++) {
+//         if (i < 2) {
+//             fib[i] = 1;
+//         } else {
+//             fib[i] = fib[i-2] + fib[i-1];
+//         }
+//     }
+//     return fib[number - 1];
+// };
+
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS";
+    if (number === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < number; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
     }
-    return fib[number - 1];
-};
+    return b;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
