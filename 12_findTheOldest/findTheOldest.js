e07c16a76ca7ef9352834197702055cf7528711e
michael-hoang <mike081522@gmail.com>
2023-04-15 11:53:56 -0700

    Complete findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    let oldestPerson = people.reduce((personA, personB) => {
+        if (!('yearOfDeath' in personA)) {
+            personA.yearOfDeath = currentYear
+        }
+        if (!('yearOfDeath' in personB)) {
+            personB.yearOfDeath = currentYear
+        }
+        let personA_age = personA.yearOfDeath - personA.yearOfBirth;
+        let personB_age = personB.yearOfDeath - personB.yearOfBirth;
+        if (personA_age >= personB_age) {
+            return personA;
+        } else { 
+            return personB;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
