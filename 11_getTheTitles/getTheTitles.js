5806360d8a0accb7a4935348204012eeba3c2c68
michael-hoang <mike081522@gmail.com>
2023-04-15 10:15:47 -0700

    Complete getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books_arr) {
+    let books = [];
+    for (let book of books_arr) {
+        books.push(book['title'])
+    }
+    return books;
 };
 
 // Do not edit below this line
